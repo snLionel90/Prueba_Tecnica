d8e32dc92abf304ad8e4479b5afb3a1b6ed57675
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2022-02-23 14:33:53 +0100

    uploaded files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require('dotenv')
+const radarRouter = require('./routes/radar')
+const getIntro = require("./routes/intro")
+
+const app = express()
+dotenv.config()
+PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+
+//routes
+app.use("/radar", radarRouter)
+app.use("/", getIntro)
+app.listen(PORT || 8801, () => {
+  console.log(`Listen on port ${PORT || 8801}`)
+})
